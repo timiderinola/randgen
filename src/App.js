2903280755c776c1394082e1198b20e62f6a4a5f
Timilehin Aderinola <timiderinola@gmail.com>
2021-03-12 17:05:08 +0800

    front end sketch

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import Button from 'react-bootstrap/Button';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container main">
+        <div id="app-card" className="card">
+          <div className="card-body">
+            <Button variant="primary">Generate</Button>
+            <div className="link">
+              <label>Link: </label>
+              <a href="" className="card-link">Link</a>
+            </div>
+            <Button variant="primary">Report</Button>
+            <div className="report">
+              <ul>
+                <li>Alphabetical string: </li>
+                <li>Real numbers: </li>
+                <li>Integers: </li>
+                <li>Alphanumerics: </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+
     </div>
   );
 }
